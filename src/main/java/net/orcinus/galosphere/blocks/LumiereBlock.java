d96be30885e6f76a429eda1400bfc12475a79c28
0rc1nus <mitchellctlui@gmail.com>
2022-05-25 16:40:42 +0800

    🤌I love mi italians

--- src/main/java/net/orcinus/galosphere/blocks/LumiereBlock.java
+++ src/main/java/net/orcinus/galosphere/blocks/LumiereBlock.java
@@ -1,5 +1,7 @@
 package net.orcinus.galosphere.blocks;
 
+import com.google.common.collect.Maps;
+import net.minecraft.Util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
@@ -8,18 +10,29 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.AmethystBlock;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.orcinus.galosphere.init.GBlocks;
 
+import java.util.Map;
 import java.util.Random;
 
 public class LumiereBlock extends AmethystBlock {
+    private static final Map<Block, Block> BLOCK_REFORMING_MAP = Util.make(Maps.newHashMap(), map -> {
+        map.put(Blocks.GRANITE, Blocks.RED_SAND);
+        map.put(Blocks.RED_SAND, Blocks.SAND);
+    });
     private final boolean charged;
 
     public LumiereBlock(boolean charged, Properties properties) {
         super(properties);
         this.charged = charged;
+    }
+
+    @Override
+    public boolean isRandomlyTicking(BlockState pState) {
+        return this.charged;
     }
 
     @Override
@@ -30,17 +43,21 @@ public class LumiereBlock extends AmethystBlock {
     @Override
     public void tick(BlockState state, ServerLevel world, BlockPos pos, Random random) {
         BlockPos abovePos = pos.above();
-        if (this.charged && world.getBlockState(abovePos).is(Blocks.GRANITE)) {
-            world.playSound(null, pos, SoundEvents.ZOMBIE_VILLAGER_CURE, SoundSource.BLOCKS, 0.5F, 1.4F);
-            world.setBlockAndUpdate(abovePos, Blocks.RED_SAND.defaultBlockState());
-            if (world.getBlockState(abovePos).isCollisionShapeFullBlock(world, abovePos)) {
-                world.levelEvent(2009, abovePos, 0);
-            }
-            world.setBlockAndUpdate(pos, GBlocks.LUMIERE_BLOCK.get().defaultBlockState());
-            for (Direction direction : Direction.Plane.HORIZONTAL) {
-                BlockPos dirPos = pos.below().relative(direction);
-                if (world.getBlockState(dirPos).isCollisionShapeFullBlock(world, dirPos)) {
-                    world.levelEvent(2009, dirPos, 0);
+        if (this.charged) {
+            for (Block block : BLOCK_REFORMING_MAP.keySet()) {
+                if (world.getBlockState(abovePos).is(block)) {
+                    world.playSound(null, pos, SoundEvents.FIRE_EXTINGUISH, SoundSource.BLOCKS, 0.75F, 1.4F);
+                    world.setBlockAndUpdate(abovePos, BLOCK_REFORMING_MAP.get(block).defaultBlockState());
+                    if (world.getBlockState(abovePos).isCollisionShapeFullBlock(world, abovePos)) {
+                        world.levelEvent(2009, abovePos, 0);
+                    }
+                    world.setBlockAndUpdate(pos, GBlocks.LUMIERE_BLOCK.get().defaultBlockState());
+                    for (Direction direction : Direction.Plane.HORIZONTAL) {
+                        BlockPos dirPos = pos.below().relative(direction);
+                        if (world.getBlockState(dirPos).isCollisionShapeFullBlock(world, dirPos)) {
+                            world.levelEvent(2009, dirPos, 0);
+                        }
+                    }
                 }
             }
         }
