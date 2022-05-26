c162401caf70c206230bfb101730639c17cdb0f0
0rc1nus <mitchellctlui@gmail.com>
2022-05-26 11:39:54 +0800

    More datagen

--- src/main/java/net/orcinus/galosphere/datagen/GItemTagsProvider.java
+++ src/main/java/net/orcinus/galosphere/datagen/GItemTagsProvider.java
@@ -2,10 +2,12 @@ package net.orcinus.galosphere.datagen;
 
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.item.Items;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.orcinus.galosphere.Galosphere;
 import net.orcinus.galosphere.init.GItemTags;
+import net.orcinus.galosphere.init.GItems;
 import org.jetbrains.annotations.Nullable;
 
 public class GItemTagsProvider extends ItemTagsProvider {
@@ -17,5 +19,6 @@ public class GItemTagsProvider extends ItemTagsProvider {
     @Override
     protected void addTags() {
         this.tag(GItemTags.SPARKLE_TEMPT_ITEMS).add(Items.GLOW_LICHEN);
+        this.tag(ItemTags.FREEZE_IMMUNE_WEARABLES).add(GItems.STERLING_HELMET.get(), GItems.STERLING_CHESTPLATE.get(), GItems.STERLING_LEGGINGS.get(), GItems.STERLING_BOOTS.get(), GItems.STERLING_HORSE_ARMOR.get());
     }
 }
