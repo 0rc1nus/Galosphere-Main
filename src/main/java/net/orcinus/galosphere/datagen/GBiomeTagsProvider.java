6d4f2ce0fb4055ef3adf153c4bb3c6f94621c1e5
0rc1nus <mitchellctlui@gmail.com>
2022-09-02 08:04:05 +0800

    Readded Tags

--- src/main/java/net/orcinus/galosphere/datagen/GBiomeTagsProvider.java
+++ src/main/java/net/orcinus/galosphere/datagen/GBiomeTagsProvider.java
@@ -3,7 +3,9 @@ package net.orcinus.galosphere.datagen;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BiomeTagsProvider;
 import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.biome.Biome;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
 import net.orcinus.galosphere.Galosphere;
 import net.orcinus.galosphere.init.GBiomes;
 import org.jetbrains.annotations.Nullable;
@@ -16,8 +18,10 @@ public class GBiomeTagsProvider extends BiomeTagsProvider {
 
     @Override
     protected void addTags() {
-        this.tag(BiomeTags.HAS_MINESHAFT).add(GBiomes.CRYSTAL_CANYONS.get());
-        this.tag(BiomeTags.HAS_RUINED_PORTAL_STANDARD).add(GBiomes.CRYSTAL_CANYONS.get());
-        this.tag(BiomeTags.HAS_STRONGHOLD).add(GBiomes.CRYSTAL_CANYONS.get());
+        for (RegistryObject<Biome> biome : GBiomes.BIOMES.getEntries()) {
+            this.tag(BiomeTags.HAS_MINESHAFT).add(biome.get());
+            this.tag(BiomeTags.HAS_RUINED_PORTAL_STANDARD).add(biome.get());
+            this.tag(BiomeTags.HAS_STRONGHOLD).add(biome.get());
+        }
     }
 }
