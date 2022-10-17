3e34b333d5ffe6787233c8836ec962bef6152360
0rc1nus <mitchellctlui@gmail.com>
2022-10-17 20:02:07 +0800

    Update ThrownEnderpearlMixin.java

--- src/main/java/net/orcinus/galosphere/mixin/ThrownEnderpearlMixin.java
+++ src/main/java/net/orcinus/galosphere/mixin/ThrownEnderpearlMixin.java
@@ -10,7 +10,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.HitResult;
-import net.minecraft.world.phys.Vec3;
 import net.orcinus.galosphere.blocks.WarpedAnchorBlock;
 import net.orcinus.galosphere.init.GBlocks;
 import net.orcinus.galosphere.util.DistanceComparator;
