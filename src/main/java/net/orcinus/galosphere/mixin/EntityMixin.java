b3ee6b916c4ba32a37e726784f4f81cd1a474d89
0rc1nus <mitchellctlui@gmail.com>
2022-11-02 18:01:48 +0800

    1.18.2

---
+++ src/main/java/net/orcinus/galosphere/mixin/EntityMixin.java
@@ -0,0 +1,39 @@
+package net.orcinus.galosphere.mixin;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.orcinus.galosphere.init.GItems;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(Entity.class)
+public class EntityMixin {
+
+    @ModifyVariable(at = @At("HEAD"), method = "turn", ordinal = 0, argsOnly = true)
+    private double I$XRotate(double value) {
+        return this.spectrePerspectiveValue(value);
+    }
+
+    @ModifyVariable(at = @At("HEAD"), method = "turn", ordinal = 1, argsOnly = true)
+    private double I$YRotate(double value) {
+        return this.spectrePerspectiveValue(value);
+    }
+
+    @Unique
+    private double spectrePerspectiveValue(double value) {
+        Entity $this = (Entity) (Object) this;
+        boolean flag = $this instanceof Player player && this.isFirstPerspective() && player.getUseItem().is(GItems.SPECTRE_BOUND_SPYGLASS.get()) && player.isUsingItem();
+        return flag ? value * 8 : value;
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    private boolean isFirstPerspective() {
+        return Minecraft.getInstance().options.getCameraType().isFirstPerson();
+    }
+
+}
