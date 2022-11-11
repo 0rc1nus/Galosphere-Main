d12f88b3e3aeb066c1fe3dcf33cedd7a84d1d416
0rc1nus <mitchellctlui@gmail.com>
2022-11-11 15:20:43 +0800

    Update FlyWanderGoal.java

--- src/main/java/net/orcinus/galosphere/entities/ai/FlyWanderGoal.java
+++ src/main/java/net/orcinus/galosphere/entities/ai/FlyWanderGoal.java
@@ -10,34 +10,34 @@ import org.jetbrains.annotations.Nullable;
 import java.util.EnumSet;
 
 public class FlyWanderGoal extends Goal {
-    private final SpectreEntity lubricEntity;
+    private final SpectreEntity spectre;
 
-    public FlyWanderGoal(SpectreEntity lubricEntity) {
-        this.lubricEntity = lubricEntity;
+    public FlyWanderGoal(SpectreEntity spectre) {
+        this.spectre = spectre;
         this.setFlags(EnumSet.of(Flag.MOVE));
     }
 
     @Override
     public boolean canUse() {
-        return this.lubricEntity.getNavigation().isDone() && this.lubricEntity.getRandom().nextInt(10) == 0;
+        return this.spectre.getNavigation().isDone() && this.spectre.getRandom().nextInt(10) == 0;
     }
 
     @Override
     public boolean canContinueToUse() {
-        return this.lubricEntity.getNavigation().isInProgress();
+        return this.spectre.getNavigation().isInProgress();
     }
 
     @Override
     public void start() {
         Vec3 vec3 = this.findPos();
         if (vec3 != null) {
-            this.lubricEntity.getNavigation().moveTo(this.lubricEntity.getNavigation().createPath(new BlockPos(vec3), 1), 1.0);
+            this.spectre.getNavigation().moveTo(this.spectre.getNavigation().createPath(new BlockPos(vec3), 1), 1.0);
         }
     }
 
     @Nullable
     private Vec3 findPos() {
-        Vec3 vec3 = this.lubricEntity.getViewVector(0.0f);
-        return AirAndWaterRandomPos.getPos(this.lubricEntity, 10, 7, -2, vec3.x, vec3.z, 1.5707963705062866);
+        Vec3 vec3 = this.spectre.getViewVector(0.0f);
+        return AirAndWaterRandomPos.getPos(this.spectre, 10, 7, -2, vec3.x, vec3.z, 1.5707963705062866);
     }
 }
