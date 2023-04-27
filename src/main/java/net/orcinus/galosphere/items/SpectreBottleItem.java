8aabc02a61f11b2f28def30eb204d599640fa081
0rc1nus <mitchellctlui@gmail.com>
2023-04-27 13:43:46 +0800

    Updated spectre bottle

--- src/main/java/net/orcinus/galosphere/items/SpectreBottleItem.java
+++ src/main/java/net/orcinus/galosphere/items/SpectreBottleItem.java
@@ -1,12 +1,11 @@
 package net.orcinus.galosphere.items;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
@@ -21,6 +20,8 @@ import net.orcinus.galosphere.entities.SpectreEntity;
 import net.orcinus.galosphere.init.GEntityTypes;
 import net.orcinus.galosphere.init.GSoundEvents;
 
+import java.util.Optional;
+
 public class SpectreBottleItem extends Item {
 
     public SpectreBottleItem(Properties properties) {
@@ -34,7 +35,6 @@ public class SpectreBottleItem extends Item {
         Player playerEntity = useOnContext.getPlayer();
         BlockPos blockPos = itemPlacementContext.getClickedPos();
         ItemStack stack = useOnContext.getItemInHand();
-        CompoundTag compoundTag = stack.getOrCreateTag();
         if (world instanceof ServerLevel serverWorld) {
             if (!playerEntity.getAbilities().instabuild) {
                 stack.shrink(1);
@@ -42,15 +42,26 @@ public class SpectreBottleItem extends Item {
             if (!playerEntity.getInventory().add(new ItemStack(Items.GLASS_BOTTLE))) {
                 playerEntity.drop(new ItemStack(Items.GLASS_BOTTLE), false);
             }
-            SpectreEntity fay = GEntityTypes.SPECTRE.create(world);
-            fay.setPos(blockPos.getX() + 0.5, blockPos.getY(), blockPos.getZ() + 0.5);
-            world.playSound(null, blockPos, GSoundEvents.SPECTRE_BOTTLE_EMPTY, SoundSource.NEUTRAL, 1.0F, 1.0F);
-            Entity entity = fay.getType().spawn(serverWorld, stack, null, blockPos, MobSpawnType.SPAWN_EGG, true, false);
-            if (entity instanceof SpectreEntity fay1) {
-                BottlePickable.loadDefaultDataFromBottleTag(fay1, compoundTag);
-                fay1.setFromBottle(true);
+            if (stack.getTag() != null && !stack.getTag().isEmpty()) {
+                Optional<Entity> entity1 = EntityType.create(stack.getTag(), world);
+                entity1.ifPresent(entity -> {
+                    entity.setPos(blockPos.getX() + 0.5D, blockPos.getY(), blockPos.getZ() + 0.5D);
+                    serverWorld.addWithUUID(entity);
+                    if (entity instanceof BottlePickable bottlePickable) {
+                        bottlePickable.setFromBottle(true);
+                    }
+                });
+            } else {
+                SpectreEntity spectre = GEntityTypes.SPECTRE.create(world);
+                spectre.setPos(blockPos.getX() + 0.5D, blockPos.getY(), blockPos.getZ() + 0.5D);
+                spectre.setFromBottle(true);
+                Entity entity = spectre.getType().spawn(serverWorld, stack, null, blockPos, MobSpawnType.SPAWN_EGG, true, false);
+                if (entity instanceof BottlePickable bottlePickable) {
+                    bottlePickable.setFromBottle(true);
+                }
+                world.playSound(null, blockPos, GSoundEvents.SPECTRE_BOTTLE_EMPTY, SoundSource.NEUTRAL, 1.0F, 1.0F);
+                world.gameEvent(useOnContext.getPlayer(), GameEvent.ENTITY_PLACE, blockPos);
             }
-            world.gameEvent(useOnContext.getPlayer(), GameEvent.ENTITY_PLACE, blockPos);
             return InteractionResult.SUCCESS;
         }
         return InteractionResult.sidedSuccess(world.isClientSide);
