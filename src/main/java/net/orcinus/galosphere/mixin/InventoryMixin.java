a9ffda8d2107363f85c986e88e892f9a1c2e9c98
0rc1nus <mitchellctlui@gmail.com>
2023-06-23 08:18:19 +1000

    Persevered Template

---
+++ src/main/java/net/orcinus/galosphere/mixin/InventoryMixin.java
@@ -0,0 +1,18 @@
+package net.orcinus.galosphere.mixin;
+
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.item.ItemStack;
+import net.orcinus.galosphere.Galosphere;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(Inventory.class)
+public class InventoryMixin {
+
+    @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/world/item/ItemStack;isEmpty()Z"), method = "dropAll")
+    private boolean G$isEmpty(ItemStack instance) {
+        return instance.getTag() != null && instance.getTag().contains("Persevered");
+    }
+
+}
--- src/main/java/net/orcinus/galosphere/mixin/ServerPlayerMixin.java
+++ src/main/java/net/orcinus/galosphere/mixin/ServerPlayerMixin.java
@@ -14,23 +14,16 @@ public class ServerPlayerMixin {
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/level/ServerPlayer;getEntityData()Lnet/minecraft/network/syncher/SynchedEntityData;", ordinal = 1, shift = At.Shift.BEFORE), method = "restoreFrom")
     private void G$restoreFrom(ServerPlayer serverPlayer, boolean bl, CallbackInfo ci) {
         ServerPlayer $this = (ServerPlayer) (Object) this;
-        for (ItemStack stack : serverPlayer.getArmorSlots()) {
-            CompoundTag tag = stack.getTag();
-            boolean flag = !stack.isEmpty() && tag != null && tag.contains("Persevered");
-            if (!flag) {
+        for (ItemStack itemStack : serverPlayer.getInventory().items) {
+            CompoundTag tag = itemStack.getTag();
+            boolean stackFlag = tag != null && tag.contains("Persevered");
+            if (!stackFlag) {
                 continue;
-            }
-            for (ItemStack itemStack : serverPlayer.getInventory().items) {
-                boolean stackFlag = itemStack.getTag() != null && itemStack.getTag().contains("Persevered");
-                if (!stackFlag) {
-                    continue;
-                }
-                $this.getInventory().add(itemStack);
             }
             if (tag.contains("Persevered")) {
                 tag.remove("Persevered");
-                break;
             }
+            $this.getInventory().add(itemStack);
         }
     }
 
