7ec7d92e80af3edb282634ec0dcae4f978fda895
0rc1nus <mitchellctlui@gmail.com>
2023-08-17 15:41:36 +0800

    Fixed composting

--- src/main/java/net/orcinus/galosphere/init/GVanillaIntegration.java
+++ src/main/java/net/orcinus/galosphere/init/GVanillaIntegration.java
@@ -1,22 +1,19 @@
 package net.orcinus.galosphere.init;
 
 import com.google.common.collect.ImmutableMap;
-import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
 import net.fabricmc.fabric.api.registry.CompostingChanceRegistry;
 import net.fabricmc.fabric.api.registry.FabricBrewingRecipeRegistry;
 import net.minecraft.Util;
-import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.alchemy.Potions;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.DispenserBlock;
-import net.orcinus.galosphere.crafting.MonstrometerDispenseItemBehavior;
 import net.orcinus.galosphere.crafting.GlowFlareDispenseItemBehavior;
 import net.orcinus.galosphere.crafting.LumiereComposterDispenseItemBehavior;
+import net.orcinus.galosphere.crafting.MonstrometerDispenseItemBehavior;
 import net.orcinus.galosphere.crafting.PickaxeDispenseItemBehavior;
 import net.orcinus.galosphere.crafting.WarpedAnchorDispenseItemBehavior;
 
@@ -35,12 +32,12 @@ public class GVanillaIntegration {
 
     public static void registerCompostables() {
         CompostingChanceRegistry instance = CompostingChanceRegistry.INSTANCE;
-        Util.make(ImmutableMap.<Block, Float>builder(), map -> {
-            map.put(GBlocks.LICHEN_MOSS, 0.85F);
-            map.put(GBlocks.BOWL_LICHEN, 0.65F);
-            map.put(GBlocks.LICHEN_ROOTS, 0.3F);
-            map.put(GBlocks.LICHEN_SHELF, 0.45F);
-            map.put(GBlocks.LICHEN_CORDYCEPS, 0.4F);
+        Util.make(ImmutableMap.<Item, Float>builder(), map -> {
+            map.put(GBlocks.LICHEN_MOSS.asItem(), 0.85F);
+            map.put(GBlocks.BOWL_LICHEN.asItem(), 0.65F);
+            map.put(GBlocks.LICHEN_ROOTS.asItem(), 0.3F);
+            map.put(GBlocks.LICHEN_SHELF.asItem(), 0.45F);
+            map.put(GBlocks.LICHEN_CORDYCEPS.asItem(), 0.4F);
         }).build().forEach(instance::add);
     }
 
